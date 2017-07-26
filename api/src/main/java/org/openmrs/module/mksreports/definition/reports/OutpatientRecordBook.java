e81a9b52fad71b08414d1fd8b4f1015010c8593f
mksrom <romain@mekomsolutions.com>
2017-07-26 14:52:43 +0700

    TLC-17: Filter rows using BasicVisitQuery

--- api/src/main/java/org/openmrs/module/mksreports/definition/reports/OutpatientRecordBook.java
+++ api/src/main/java/org/openmrs/module/mksreports/definition/reports/OutpatientRecordBook.java
@@ -11,7 +11,6 @@ import org.openmrs.PatientIdentifierType;
 import org.openmrs.api.PatientService;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.mksreports.MKSReportsConstants;
-import org.openmrs.module.mksreports.query.VisitWithinDateRangeQuery;
 import org.openmrs.module.reporting.common.Age;
 import org.openmrs.module.reporting.common.AgeRange;
 import org.openmrs.module.reporting.common.MessageUtil;
@@ -25,6 +24,7 @@ import org.openmrs.module.reporting.data.visit.library.BuiltInVisitDataLibrary;
 import org.openmrs.module.reporting.dataset.definition.VisitDataSetDefinition;
 import org.openmrs.module.reporting.evaluation.parameter.Mapped;
 import org.openmrs.module.reporting.evaluation.parameter.Parameter;
+import org.openmrs.module.reporting.query.visit.definition.BasicVisitQuery;
 import org.openmrs.module.reporting.query.visit.definition.VisitQuery;
 import org.openmrs.module.reporting.report.ReportDesign;
 import org.openmrs.module.reporting.report.definition.ReportDefinition;
@@ -35,39 +35,39 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class OutpatientRecordBook extends BaseReportManager {
-	
+
 	@Autowired
 	private PatientService patientService;
-	
+
 	@Autowired
 	private BuiltInPatientDataLibrary builtInPatientData;
-	
+
 	@Autowired
 	private BuiltInVisitDataLibrary builtInVisitData;
-	
+
 	@Override
 	public String getUuid() {
 		return "6c74e2ab-0e9b-4469-8901-8221f7d4b498";
 	}
-	
+
 	@Override
 	public String getName() {
 		return "HIS Outpatient Record Book";
 	}
-	
+
 	@Override
 	public String getDescription() {
 		return "";
 	}
-	
+
 	private Parameter getStartDateParameter() {
 		return new Parameter("startDate", "Start Date", Date.class);
 	}
-	
+
 	private Parameter getEndDateParameter() {
 		return new Parameter("endDate", "End Date", Date.class);
 	}
-	
+
 	@Override
 	public List<Parameter> getParameters() {
 		List<Parameter> params = new ArrayList<Parameter>();
@@ -75,50 +75,61 @@ public class OutpatientRecordBook extends BaseReportManager {
 		params.add(getEndDateParameter());
 		return params;
 	}
-	
+
 	public OutpatientRecordBook() {
 	};
-	
+
 	@Override
 	public ReportDefinition constructReportDefinition() {
-		
+
 		ReportDefinition rd = new ReportDefinition();
 		rd.setUuid(getUuid());
 		rd.setName(getName());
 		rd.setDescription(getDescription());
-		
+
 		rd.setParameters(getParameters());
-		
+
 		VisitDataSetDefinition vdsd = new VisitDataSetDefinition();
 		vdsd.addParameters(getParameters());
 		rd.addDataSetDefinition("visits", Mapped.mapStraightThrough(vdsd));
-		
-		VisitWithinDateRangeQuery query = new VisitWithinDateRangeQuery();
-		query.setParameters(getParameters());
-		vdsd.addRowFilter(query, ObjectUtil.toString(Mapped.straightThroughMappings(query), "=", ","));
-		
+
+		BasicVisitQuery query = new BasicVisitQuery();
+
+		Parameter endedOnOrAfter = new Parameter("endedOnOrAfter", "Ended On Or After", Date.class);
+		Parameter endedBefore = new Parameter("endedBefore", "Ended Before", Date.class);
+		query.setParameters(Arrays.asList(endedOnOrAfter, endedBefore));
+
+		Mapped<BasicVisitQuery> mappedVQ = new Mapped<BasicVisitQuery>();
+		mappedVQ.setParameterizable(query);
+		Map<String, Object> parameterMappings = new HashMap<String, Object>();
+		parameterMappings.put("endedOnOrAfter", "${startDate}");
+		parameterMappings.put("endedBefore", "${endDate}");
+		mappedVQ.setParameterMappings(parameterMappings);
+
+		vdsd.addRowFilter(query, ObjectUtil.toString(parameterMappings, "=", ","));
+
 		// Visit ID
 		VisitDataDefinition vdd = builtInVisitData.getVisitId();
 		vdsd.addColumn("Visit ID", vdd, ObjectUtil.toString(Mapped.straightThroughMappings(vdd), "=", ","));
-		
+
 		// Patient ID
 		vdsd.addColumn("Patient ID", builtInPatientData.getPatientId(),
-		    ObjectUtil.toString(Mapped.straightThroughMappings(builtInPatientData.getPatientId()), "=", ","));
-		
+				ObjectUtil.toString(Mapped.straightThroughMappings(builtInPatientData.getPatientId()), "=", ","));
+
 		// Patient Identifier
 		PatientIdentifierType type = patientService.getPatientIdentifierTypeByUuid(Context.getAdministrationService()
-		        .getGlobalProperty(MKSReportsConstants.GP_PATIENT_IDENTIFIER_TYPE_UUID));
+				.getGlobalProperty(MKSReportsConstants.GP_PATIENT_IDENTIFIER_TYPE_UUID));
 		PatientIdentifierDataDefinition pidd = new PatientIdentifierDataDefinition();
 		pidd.setTypes(Arrays.asList(type));
-		
-		vdsd.addColumn(MessageUtil.translate("${project.parent.artifactId}.report.registerLogbook.identifier.label"), pidd,
-		    ObjectUtil.toString(Mapped.straightThroughMappings(pidd), "=", ","));
-		
+
+		vdsd.addColumn(MessageUtil.translate("mksreports.report.outpatientRecordBook.identifier.label"), pidd,
+				ObjectUtil.toString(Mapped.straightThroughMappings(pidd), "=", ","));
+
 		// Age Categories
 		AgeDataDefinition ageDD = new AgeDataDefinition();
-		
-		String isOfCategoryLabel = MessageUtil.translate("mksreports.report.registerLogbook.isOfCategory.label");
-		
+
+		String isOfCategoryLabel = MessageUtil.translate("mksreports.report.outpatientRecordBook.isOfCategory.label");
+
 		AgeRangeConverter ageConverter1 = new AgeRangeConverter();
 		ageConverter1.addAgeRange(new AgeRange(0, Age.Unit.MONTHS, 1, Age.Unit.MONTHS, isOfCategoryLabel));
 		AgeRangeConverter ageConverter2 = new AgeRangeConverter();
@@ -135,39 +146,39 @@ public class OutpatientRecordBook extends BaseReportManager {
 		ageConverter7.addAgeRange(new AgeRange(50, Age.Unit.YEARS, 65, Age.Unit.YEARS, isOfCategoryLabel));
 		AgeRangeConverter ageConverter8 = new AgeRangeConverter();
 		ageConverter8.addAgeRange(new AgeRange(65, Age.Unit.YEARS, 999, Age.Unit.YEARS, isOfCategoryLabel));
-		
-		vdsd.addColumn(MessageUtil.translate("mksreports.report.registerLogbook.ageCategory1.label"), ageDD, (String) null,
-		    ageConverter1);
-		vdsd.addColumn(MessageUtil.translate("mksreports.report.registerLogbook.ageCategory2.label"), ageDD, (String) null,
-		    ageConverter2);
-		vdsd.addColumn(MessageUtil.translate("mksreports.report.registerLogbook.ageCategory3.label"), ageDD, (String) null,
-		    ageConverter3);
-		vdsd.addColumn(MessageUtil.translate("mksreports.report.registerLogbook.ageCategory4.label"), ageDD, (String) null,
-		    ageConverter4);
-		vdsd.addColumn(MessageUtil.translate("mksreports.report.registerLogbook.ageCategory5.label"), ageDD, (String) null,
-		    ageConverter5);
-		vdsd.addColumn(MessageUtil.translate("mksreports.report.registerLogbook.ageCategory6.label"), ageDD, (String) null,
-		    ageConverter6);
-		vdsd.addColumn(MessageUtil.translate("mksreports.report.registerLogbook.ageCategory7.label"), ageDD, (String) null,
-		    ageConverter7);
-		vdsd.addColumn(MessageUtil.translate("mksreports.report.registerLogbook.ageCategory8.label"), ageDD, (String) null,
-		    ageConverter8);
-		
+
+		vdsd.addColumn(MessageUtil.translate("mksreports.report.outpatientRecordBook.ageCategory1.label"), ageDD, (String) null,
+				ageConverter1);
+		vdsd.addColumn(MessageUtil.translate("mksreports.report.outpatientRecordBook.ageCategory2.label"), ageDD, (String) null,
+				ageConverter2);
+		vdsd.addColumn(MessageUtil.translate("mksreports.report.outpatientRecordBook.ageCategory3.label"), ageDD, (String) null,
+				ageConverter3);
+		vdsd.addColumn(MessageUtil.translate("mksreports.report.outpatientRecordBook.ageCategory4.label"), ageDD, (String) null,
+				ageConverter4);
+		vdsd.addColumn(MessageUtil.translate("mksreports.report.outpatientRecordBook.ageCategory5.label"), ageDD, (String) null,
+				ageConverter5);
+		vdsd.addColumn(MessageUtil.translate("mksreports.report.outpatientRecordBook.ageCategory6.label"), ageDD, (String) null,
+				ageConverter6);
+		vdsd.addColumn(MessageUtil.translate("mksreports.report.outpatientRecordBook.ageCategory7.label"), ageDD, (String) null,
+				ageConverter7);
+		vdsd.addColumn(MessageUtil.translate("mksreports.report.outpatientRecordBook.ageCategory8.label"), ageDD, (String) null,
+				ageConverter8);
+
 		return rd;
 	}
-	
+
 	@Override
 	public List<ReportDesign> constructReportDesigns(ReportDefinition reportDefinition) {
 		ReportDesign design = ReportManagerUtil.createCsvReportDesign("9873e45d-f8a0-4682-be78-243b8c9b848c",
-		    reportDefinition);
+				reportDefinition);
 		List<ReportDesign> list = new ArrayList<ReportDesign>();
 		list.add(design);
 		return list;
 	}
-	
+
 	@Override
 	public String getVersion() {
 		return "0.1.0-SNAPSHOT";
 	}
-	
+
 }
